22620fe1d890dc830770de12bfba86ba558d9320
YSfallingStar <wjddbtjd55@naver.com>
2022-03-28 09:38:16 +0900

    [chatting] socket

--- src/components/pages/Chat.js
+++ src/components/pages/Chat.js
@@ -5,47 +5,47 @@ import { socket, SocketContext, SOCKET_EVENT } from "../../service/socket";
 import { Box } from "@mui/material";
 
 function Chat({ handleClose }) {
-    const prevNickname = useRef(null); // prevNickname 변경은 컴포넌트를 리렌더링 하지않습니다.
-    const [nickname, setNickname] = useState("OOO");
+  const prevNickname = useRef(null); // prevNickname 변경은 컴포넌트를 리렌더링 하지않습니다.
+  const [nickname, setNickname] = useState("OOO");
 
-    useEffect(() => {
-        return () => { // App 컴포넌트 unmount시 실행
-            socket.disconnect();
-        }
-    }, []);
+  useEffect(() => {
+    return () => { // App 컴포넌트 unmount시 실행
+      socket.disconnect();
+    }
+  }, []);
 
-    useEffect(() => {
-        socket.emit(SOCKET_EVENT.JOIN_ROOM, { nickname }); // JOIN_ROOM event type과 nickname data를 서버에 전송한다.
-    }, [nickname]);
+  useEffect(() => {
+    socket.emit(SOCKET_EVENT.JOIN_ROOM, { nickname }); // JOIN_ROOM event type과 nickname data를 서버에 전송한다.
+  }, [nickname]);
 
-    useEffect(() => {
-        if (prevNickname.current) {
-            socket.emit(SOCKET_EVENT.UPDATE_NICKNAME, { // 서버에는 이전 닉네임과 바뀐 닉네임을 전송해줍니다.
-                prevNickname: prevNickname.current,
-                nickname,
-            });
-        } else {
-            socket.emit(SOCKET_EVENT.JOIN_ROOM, { nickname });
-        }
-    }, [nickname]);
+  useEffect(() => {
+    if (prevNickname.current) {
+      socket.emit(SOCKET_EVENT.UPDATE_NICKNAME, { // 서버에는 이전 닉네임과 바뀐 닉네임을 전송해줍니다.
+        prevNickname: prevNickname.current,
+        nickname,
+      });
+    } else {
+      socket.emit(SOCKET_EVENT.JOIN_ROOM, { nickname });
+    }
+  }, [nickname]);
 
-    const handleSubmitNickname = useCallback(newNickname => {
-        prevNickname.current = nickname;
-        setNickname(newNickname);
-    },
-        [nickname]
-    );
+  const handleSubmitNickname = useCallback(newNickname => {
+    prevNickname.current = nickname;
+    setNickname(newNickname);
+  },
+    [nickname]
+  );
 
-    return (
-        <Box>
-            <SocketContext.Provider value={socket} >
-                <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-                    <NicknameForm handleSubmitNickname={handleSubmitNickname} handleClose={handleClose} />
-                    <ChatRoom nickname={nickname} />
-                </div>
-            </SocketContext.Provider>
-        </Box>
-    );
+  return (
+    <Box>
+      <SocketContext.Provider value={socket} >
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+          <NicknameForm handleSubmitNickname={handleSubmitNickname} handleClose={handleClose} />
+          <ChatRoom nickname={nickname} />
+        </div>
+      </SocketContext.Provider>
+    </Box>
+  );
 }
 
 export default Chat;
--- src/service/socket.js
+++ src/service/socket.js
@@ -6,46 +6,46 @@ export const socket = socketIo(String(process.env.REACT_APP_BACK_URL), { withCre
 export const SocketContext = createContext(socket);
 
 export const SOCKET_EVENT = {
-    JOIN_ROOM: "JOIN_ROOM",
-    UPDATE_NICKNAME: "UPDATE_NICKNAME",
-    SEND_MESSAGE: "SEND_MESSAGE",
-    RECEIVE_MESSAGE: "RECEIVE_MESSAGE",
+  JOIN_ROOM: "JOIN_ROOM",
+  UPDATE_NICKNAME: "UPDATE_NICKNAME",
+  SEND_MESSAGE: "SEND_MESSAGE",
+  RECEIVE_MESSAGE: "RECEIVE_MESSAGE",
 };
 
 export const makeMessage = pongData => {
-    const { prevNickname, nickname, content, type, time } = pongData;
+  const { prevNickname, nickname, content, type, time } = pongData;
 
-    let nicknameLabel;
-    let contentLabel = "";
+  let nicknameLabel;
+  let contentLabel = "";
 
-    switch (type) {
-        case SOCKET_EVENT.JOIN_ROOM: {
-            contentLabel = `${nickname} has joined the room.`;
-            break;
-        }
-        case SOCKET_EVENT.UPDATE_NICKNAME: {
-            contentLabel = `User's name has been changed.\n ${prevNickname} => ${nickname}.`;
-            break;
-        }
-        case SOCKET_EVENT.SEND_MESSAGE: {
-            contentLabel = String(content);
-            nicknameLabel = nickname;
-            break;
-        }
-        default:
+  switch (type) {
+    case SOCKET_EVENT.JOIN_ROOM: {
+      contentLabel = `${nickname} has joined the room.`;
+      break;
     }
+    case SOCKET_EVENT.UPDATE_NICKNAME: {
+      contentLabel = `User's name has been changed.\n ${prevNickname} => ${nickname}.`;
+      break;
+    }
+    case SOCKET_EVENT.SEND_MESSAGE: {
+      contentLabel = String(content);
+      nicknameLabel = nickname;
+      break;
+    }
+    default:
+  }
 
-    return {
-        nickname: nicknameLabel,
-        content: contentLabel,
-        time: dayjs(time).format("HH:mm"),
-    };
+  return {
+    nickname: nicknameLabel,
+    content: contentLabel,
+    time: dayjs(time).format("HH:mm"),
+  };
 };
 
 socket.on("connect", () => {
-    console.log("socket server connected.");
+  console.log("socket server connected.");
 });
 
 socket.on("disconnect", () => {
-    console.log("socket server disconnected.");
+  console.log("socket server disconnected.");
 });
